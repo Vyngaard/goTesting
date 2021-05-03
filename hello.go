b4c55b6ff7925c7354b550eb06b557b55257756c
Vyngaard <schramdawg@gmail.com>
2021-05-03 12:11:06 -0500

    Added fucntionality for multiple languages.

--- hello.go
+++ hello.go
@@ -4,15 +4,35 @@ import "fmt"
 
 //important to refactor for efficiency
 //both the production code and the tests
-const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+//easy to read function containing our switch for language selection
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "French"))
 }
